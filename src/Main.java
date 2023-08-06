eed8b95fac2fff0f164ee95f262f344539ce5b04
Matvey Lazarev <matv26-01@mail.ru>
2023-08-06 12:31:12 +0300

    Update project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        // 1. git add Main
+        //    git commit ->  1.0.0
+
+        // 2. git add Main2
+        //    git commit ->  1.0.1
+
     }
 }
